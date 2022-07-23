3a106779ee542020fa040d7b6f8c24c24d56bd35
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-07-23 23:47:27 +0600

    finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5500
+const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
